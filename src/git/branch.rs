1f7ae717f35089294a8091e655d7480b0541cc0f
Siwei LU <me@siwei.lu>
2023-09-09 02:30:25 +0800

    feat: resolve main branch

--- src/git/branch.rs
+++ src/git/branch.rs
@@ -1,10 +1,29 @@
-use git2::{Error, BranchType};
+use git2::{BranchType, Error};
 
 use super::repo;
 
-
 pub fn main() -> String {
-    "main".to_string()
+    let repo = match repo::current() {
+        Ok(repo) => repo,
+        Err(_) => return "master".to_string(),
+    };
+
+    let refs_patterns = [
+        "refs/heads/main",
+        "refs/heads/trunk",
+        "refs/remotes/origin/main",
+        "refs/remotes/origin/trunk",
+        "refs/remotes/upstream/main",
+        "refs/remotes/upstream/trunk",
+    ];
+
+    for pattern in refs_patterns.iter() {
+        if let Ok(reference) = repo.find_reference(pattern) {
+            return reference.shorthand().unwrap_or("master").to_string();
+        }
+    }
+
+    "master".to_string()
 }
 
 pub fn new(name: &str) -> Result<(), Error> {
